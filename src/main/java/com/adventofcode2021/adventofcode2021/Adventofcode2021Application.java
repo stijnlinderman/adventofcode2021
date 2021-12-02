518dfc4888ff6a2343a222b01f82e11112c96504
Stijn <slinderman@sogyo.nl>
2021-12-02 08:36:34 +0100

    Basic spring boot app

---
+++ src/main/java/com/adventofcode2021/adventofcode2021/Adventofcode2021Application.java
@@ -0,0 +1,13 @@
+package com.adventofcode2021.adventofcode2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adventofcode2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adventofcode2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/adventofcode2021/adventofcode2021/Adventofcode2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adventofcode2021.adventofcode2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Adventofcode2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
