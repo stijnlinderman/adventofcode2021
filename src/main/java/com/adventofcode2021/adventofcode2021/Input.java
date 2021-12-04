8decdc20fdf3e49176934551928e9090c8fb9eb6
Stijn <slinderman@sogyo.nl>
2021-12-04 19:52:01 +0100

    Added input class.

---
+++ src/main/java/com/adventofcode2021/adventofcode2021/Input.java
@@ -0,0 +1,13 @@
+package com.adventofcode2021.adventofcode2021;
+
+public class Input {
+
+	private static String input = "Hello world";
+
+	public static String getInput () {
+		return input;
+	}
+
+}
+
+
