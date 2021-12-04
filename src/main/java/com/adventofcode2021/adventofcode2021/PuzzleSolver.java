42f65cd4daf8902fb5502d5c4fd5a46da96a55f3
Stijn <slinderman@sogyo.nl>
2021-12-04 19:59:45 +0100

    Added PuzzleSolver class to get and print puzzle answer.

---
+++ src/main/java/com/adventofcode2021/adventofcode2021/PuzzleSolver.java
@@ -0,0 +1,13 @@
+package com.adventofcode2021.adventofcode2021;
+
+public class PuzzleSolver {
+
+    public static String getAnswer (String input) {
+        return "Answer: " + input;
+    }
+
+    public static void print (String stringToPrint) {
+        System.out.println(stringToPrint);
+    }
+    
+}
